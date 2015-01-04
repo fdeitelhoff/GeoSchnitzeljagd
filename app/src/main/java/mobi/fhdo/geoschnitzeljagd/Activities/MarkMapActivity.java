dddabf150e3fa3daf0cbddd0b69c8ce9f4c5b95d
Fabian Deitelhoff <Fabian@FabianDeitelhoff.de>
2015-01-04 21:49:53 +0100

    Normale Activity statt der Fullscreen-Activity für das Map-Element.

--- app/src/main/java/mobi/fhdo/geoschnitzeljagd/Activities/MarkMapActivity.java
+++ app/src/main/java/mobi/fhdo/geoschnitzeljagd/Activities/MarkMapActivity.java
@@ -2,21 +2,22 @@ package mobi.fhdo.geoschnitzeljagd.Activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-import mobi.fhdo.geoschnitzeljagd.Activities.util.SystemUiHider;
 import mobi.fhdo.geoschnitzeljagd.R;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
 public class MarkMapActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_mark_map);
     }
