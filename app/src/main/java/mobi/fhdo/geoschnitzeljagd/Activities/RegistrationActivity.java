d7eb213ee1d1ff4307bfba50e13916fc6b36c2e5
Fabian Deitelhoff <Fabian@FabianDeitelhoff.de>
2015-01-06 22:37:53 +0100

    Registrierung überarbeitet. Direkte Anmeldung ist jetzt implementiert.

--- app/src/main/java/mobi/fhdo/geoschnitzeljagd/Activities/RegistrationActivity.java
+++ app/src/main/java/mobi/fhdo/geoschnitzeljagd/Activities/RegistrationActivity.java
@@ -2,7 +2,9 @@ package mobi.fhdo.geoschnitzeljagd.Activities;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -22,25 +24,19 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.UUID;
 
 import mobi.fhdo.geoschnitzeljagd.Contexts.UserContext;
 import mobi.fhdo.geoschnitzeljagd.DataManagers.Users;
-import mobi.fhdo.geoschnitzeljagd.Model.Mark;
-import mobi.fhdo.geoschnitzeljagd.Model.Paperchase;
 import mobi.fhdo.geoschnitzeljagd.Model.User;
 import mobi.fhdo.geoschnitzeljagd.R;
 
-public class RegistrationActivity extends Activity
-{
+public class RegistrationActivity extends Activity {
     private EditText registration_username;
     private EditText registration_password;
 
@@ -48,8 +44,7 @@ public class RegistrationActivity extends Activity
 
     private User newUser;
 
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
@@ -60,63 +55,64 @@ public class RegistrationActivity extends Activity
 
         Button SignInButton;
         SignInButton = (Button) findViewById(R.id.registration_button);
-        SignInButton.setOnClickListener(new OnClickListener()
-        {
+        SignInButton.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 ClickRegistration();
             }
         });
     }
 
-    public void ClickRegistration()
-    {
-        try
-        {
-            newUser = new User(registration_username.getText().toString(), UserContext.getMd5(registration_password.getText().toString()));
+    public void ClickRegistration() {
+        try {
+            String username = registration_username.getText().toString();
+            String password = registration_password.getText().toString();
 
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(getBaseContext(), "Für die Registrierung sind Benutzername und Passwort notwendig.", Toast.LENGTH_LONG).show();
+            }
+
+            newUser = new User(username, UserContext.getMd5(password));
 
             String stringUrl = "http://schnitzeljagd.fabiandeitelhoff.de/api/v1/register";
             ConnectivityManager connMgr = (ConnectivityManager)
                     getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-            if (networkInfo != null && networkInfo.isConnected())
-            {
+            if (networkInfo != null && networkInfo.isConnected()) {
                 // User Lokal anlegen
-                if (users.CreateOrUpdate(newUser))
-                {
-                    Toast.makeText(getBaseContext(), "Erfolgreich", Toast.LENGTH_LONG).show();
-                } else
-                {
+                if (users.CreateOrUpdate(newUser)) {
+                    UserContext.getInstance().userLoggedIn(users.Login(newUser));
+
+                    // User auf dem Server hinterlegen
+                    new DownloadWebpageTask().execute(stringUrl);
+
+                    new AlertDialog.Builder(this)
+                            .setTitle("Registrierung erfolgreich")
+                            .setMessage("Sie werden nun automatisch angemeldet.")
+                            .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    Intent intent = new Intent(getBaseContext(), HomeActivity.class);
+                                    startActivity(intent);
+                                }
+                            })
+                            .show();
+                } else {
                     Toast.makeText(getBaseContext(), "Fehler", Toast.LENGTH_LONG).show();
                 }
-
-                // User auf dem Server hinterlegen
-                new DownloadWebpageTask().execute(stringUrl);
-            } else
-            {
+            } else {
                 Toast.makeText(getBaseContext(), "No network connection available.", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(intent);
             }
-
-
-
-
-
-            Intent myIntent = new Intent(this, LoginActivity.class);
-            startActivity(myIntent);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event)
-    {
-        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0)
-        {
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
             Intent myIntent = new Intent(this, LoginActivity.class);
             startActivity(myIntent);
             return true;
@@ -126,12 +122,10 @@ public class RegistrationActivity extends Activity
     }
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
-    private String downloadUrl(String myurl) throws IOException
-    {
+    private String downloadUrl(String myurl) throws IOException {
         InputStream is = null;
 
-        try
-        {
+        try {
             URL url = new URL(myurl);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestProperty("Content-Type", "application/json");
@@ -159,29 +153,21 @@ public class RegistrationActivity extends Activity
 
             // Makes sure that the InputStream is closed after the app is
             // finished using it.
-        }
-        finally
-        {
-            if (is != null)
-            {
+        } finally {
+            if (is != null) {
                 is.close();
             }
         }
     }
 
-    private class DownloadWebpageTask extends AsyncTask<String, Void, String>
-    {
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
         @Override
-        protected String doInBackground(String... urls)
-        {
+        protected String doInBackground(String... urls) {
 
             // params comes from the execute() call: params[0] is the url.
-            try
-            {
+            try {
                 return downloadUrl(urls[0]);
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 return "Unable to retrieve web page. URL may be invalid.";
             }
         }
@@ -189,53 +175,40 @@ public class RegistrationActivity extends Activity
         // onPostExecute displays the results of the AsyncTask.
         @TargetApi(Build.VERSION_CODES.KITKAT)
         @Override
-        protected void onPostExecute(String result)
-        {
+        protected void onPostExecute(String result) {
             Log.d("Test:", result);
-            try
-            {
+            try {
                 InputStream input = new ByteArrayInputStream(result.getBytes(StandardCharsets.UTF_8));
 
                 JsonReader reader = new JsonReader(new InputStreamReader(input, "UTF-8"));
 
                 reader.beginObject();
-                while (reader.hasNext())
-                {
+                while (reader.hasNext()) {
                     String name = reader.nextName();
-                    if (name.equals("data"))
-                    {
+                    if (name.equals("data")) {
                         reader.beginObject();
-                        while (reader.hasNext())
-                        {
+                        while (reader.hasNext()) {
                             name = reader.nextName();
-                            if (name.equals("Timestamp"))
-                            {
-                                try
-                                {
+                            if (name.equals("Timestamp")) {
+                                try {
                                     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
                                     Date parsedTimeStamp = dateFormat.parse(reader.nextString());
                                     newUser.setTimestamp(new Timestamp(parsedTimeStamp.getTime()));
 
                                     users.CreateOrUpdate(newUser);
-                                }
-                                catch (Exception e)
-                                {
+                                } catch (Exception e) {
                                     e.printStackTrace();
                                 }
-                            } else
-                            {
+                            } else {
                                 reader.skipValue();
                             }
                         }
-                    } else
-                    {
+                    } else {
                         reader.skipValue();
                     }
                 }
                 reader.endObject();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
